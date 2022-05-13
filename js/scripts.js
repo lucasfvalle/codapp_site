cd8183b531e9ec26716e8482b391cabacf56d4a1
Lucas Valle <lucasfvalle@gmail.com>
2022-05-13 17:31:49 -0300

    Responsividade e conteúdo.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
 window.addEventListener("DOMContentLoaded", function (event) {
     menu_btn = document.getElementById('toggle-btn');
     menu = document.getElementById('menu-list');
-
     menu_btn.addEventListener("click", () =>{
-        console.log("teste")
         menu.classList.toggle('active');
-        console.log(menu);
     });
-
 });
     
